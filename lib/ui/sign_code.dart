347542e296c180f70faf89e2736cdda8ef35f68c
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-05-10 07:04:57 +0100

    added digits ui

--- lib/ui/sign_code.dart
+++ lib/ui/sign_code.dart
@@ -8,6 +8,8 @@ class SignCode extends StatefulWidget {
 
 class _SignCode extends State<SignCode> {
 
+  var _inputs = List.filled(4, "X");
+
   Widget _buildCodeUI() {
     var textSentTo = Column(
       mainAxisAlignment: MainAxisAlignment.center,
@@ -16,16 +18,45 @@ class _SignCode extends State<SignCode> {
         Text(
           "Code sent by SMS to",
           style: TextStyle(
-            color: Theme.of(context).accentColor
+            color: Theme.of(context).accentColor,
+            fontSize: 18.0
           ),
         ),
         Text(
           "+33 234 556 7888",
           style: TextStyle(
-            color: Theme.of(context).accentColor
+            color: Theme.of(context).accentColor,
+            fontSize: 18.0
           ),
         )
       ],
+    );
+
+    var inputs = Container(
+      padding: EdgeInsets.all(16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: List<Widget>.generate(_inputs.length, (index) => Container(
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                width: 5.0,
+                color: Color(0xFFC6902E)
+              )
+            )
+          ),
+          padding: EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+          margin: EdgeInsets.all(5.0),
+          child: Text(
+            _inputs[index],
+            style: Theme.of(context).textTheme.headline3.copyWith(
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).primaryColor
+            )
+          ),
+        ))
+      ),
     );
 
     return Expanded(
@@ -33,10 +64,11 @@ class _SignCode extends State<SignCode> {
       child: Container(
         color: Colors.red,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            textSentTo
+            textSentTo,
+            inputs
           ],
         ),
       )
