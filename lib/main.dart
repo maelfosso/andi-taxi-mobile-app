ed568e70de71dea82d05c5078aa0dcb196f50e1c
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-05-13 21:10:12 +0100

    changed main content. added bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,44 @@
-import 'package:andi_taxi/ui/sign_up.dart';
+import 'package:andi_taxi/repository/authentication/authentication_repository.dart';
 import 'package:andi_taxi/ui/welcome.dart';
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(MyApp());
+  Bloc.observer = AppBlocObserver();
+  
+  runApp(App(
+    authenticationRepository: AuthenticationRepository(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   // This widget is the root of your application.
-  Map<int, Color> colorCodes = {
+  
+  App({
+    Key? key,
+    required AuthenticationRepository authenticationRepository
+  }): _authenticationRepository = authenticationRepository,
+      super(key: key);
+
+  final AuthenticationRepository _authenticationRepository;
+  
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: _authenticationRepository,
+      child: BlocProvider(
+        create: (_) => AppBloc(
+          authenticationRepository: _authenticationRepository
+        ),
+        child: AppView(),
+      ),
+    );
+  }
+}
+
+class AppView extends StatelessWidget {
+  final Map<int, Color> colorCodes = {
     50: Color.fromRGBO(198, 144, 46, .1),
     100: Color.fromRGBO(198, 144, 46, .2),
     200: Color.fromRGBO(198, 144, 46, .3),
@@ -21,24 +51,16 @@ class MyApp extends StatelessWidget {
     900: Color.fromRGBO(198, 144, 46, 1),
   };
 
-  
+  AppView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     MaterialColor color = new MaterialColor(0xFFC6902E, colorCodes);
 
     return MaterialApp(
-      title: 'An Di Taxi',
+      title: 'AnDi Taxi',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primaryColor: Color(0xFFC6902E),
         accentColor: Color(0xFF97ADB6),
         primarySwatch: color
