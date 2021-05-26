01c364327f09d15e482bddf97a00f5e5e0d9ca82
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-05-26 04:38:22 +0100

    added custom icon for the current location

--- lib/pages/gmap/cubit/gmap_cubit.dart
+++ lib/pages/gmap/cubit/gmap_cubit.dart
@@ -5,6 +5,7 @@ import 'package:andi_taxi/models/place.dart';
 import 'package:andi_taxi/repository/gmap/geolocation_repository.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/painting.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -16,10 +17,18 @@ class GMapCubit extends Cubit<GMapState> {
 
   late GoogleMapController gMapController;
   late StreamSubscription<Position> _positionSubscription;
+  late BitmapDescriptor currentLocationIcon;
+
 
   GMapCubit(this._geolocationRepository) : super(GMapState(markers: {})) {
     print('INTO GMapCUBIT');
 
+
+    BitmapDescriptor.fromAssetImage(
+         ImageConfiguration(devicePixelRatio: 16.0),
+         'assets/images/ic_setloc.png').then((onValue) {
+            currentLocationIcon = onValue;
+         });
     // _positionSubscription = this._geolocationRepository.position.listen(
     //   (Position position) {
     //     // the state will change with the new position
@@ -43,7 +52,8 @@ class GMapCubit extends Cubit<GMapState> {
       var latLng = LatLng(position.position.latitude, position.position.longitude);
       var marker = Marker(
         markerId: GMapState.currentLocationId,
-        position: latLng
+        position: latLng,
+        icon: currentLocationIcon
       );
 
       CameraUpdate update = CameraUpdate.newLatLngZoom(latLng, 16);
