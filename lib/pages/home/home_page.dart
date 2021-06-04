1f5c30485012aa7b421acfa54a356fb46eaf5621
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-06-04 18:32:17 +0100

    Modifying the appbar when BookingTaxi started

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,8 +1,6 @@
-import 'package:andi_taxi/blocs/app/app_bloc.dart';
-import 'package:andi_taxi/blocs/authentication/authentication_bloc.dart';
 import 'package:andi_taxi/blocs/booking_taxi/booking_taxi_bloc.dart';
 import 'package:andi_taxi/blocs/gmap/gmap_bloc.dart';
-import 'package:andi_taxi/pages/gmap/cubit/gmap_cubit.dart';
+// import 'package:andi_taxi/pages/gmap/cubit/gmap_cubit.dart';
 import 'package:andi_taxi/pages/gmap/view/gmap_page.dart';
 import 'package:andi_taxi/pages/home/home_view.dart';
 import 'package:andi_taxi/repository/booking_taxi/booking_taxi_repository.dart';
@@ -21,76 +19,11 @@ class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   static Route route() {
-    return MaterialPageRoute<void>(builder: (_) => HomePage());
-  }
-  
-  static Page page() => const MaterialPage<void>(child: HomePage());
-
-  @override
-  Widget build(BuildContext context) {
     final GeolocationRepository geolocationRepository = GeolocationRepository();
     final BookingTaxiRepository bookingTaxiRepository = BookingTaxiRepository();
 
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      
-      appBar: AppBar(
-        automaticallyImplyLeading: true,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            ConstrainedBox(
-              constraints: BoxConstraints.tightFor(width: 40, height: 40),
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  // padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-                  padding: EdgeInsets.all(0.0),
-                  primary: Colors.white,
-                  shape: CircleBorder(),
-                  elevation: 8.0
-                ),
-                child: IconButton(
-                  icon: Icon(Icons.sort),
-                  color: Color(0xFFC6902E),
-                  onPressed: () {},
-                )
-              )
-            ),
-            Text(
-              AppLocalizations.of(context)!.home,
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold
-              ),
-            ),
-            ConstrainedBox(
-              constraints: BoxConstraints.tightFor(width: 40, height: 40),
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  // padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-                  padding: EdgeInsets.all(0.0),
-                  primary: Colors.white,
-                  shape: CircleBorder(),
-                  elevation: 8.0
-                ),
-                child: IconButton(
-                  icon: Icon(Icons.exit_to_app),
-                  color: Color(0xFFC6902E),
-                  onPressed: () {},
-                )
-              )
-            )
-          ],
-        ),
-        elevation: 0.0,
-        backgroundColor: Colors.transparent,
-      ),
-      body: MultiRepositoryProvider(
+    return MaterialPageRoute<void>(builder: (_) => MultiRepositoryProvider(
         providers: [
-          // RepositoryProvider<GeolocationRepository>(create: (context) => GeolocationRepository()),
-          // RepositoryProvider<BookingTaxiRepository>(create: (context) => BookingTaxiRepository()),
           RepositoryProvider<GeolocationRepository>.value(value: geolocationRepository),
           RepositoryProvider<BookingTaxiRepository>.value(value: bookingTaxiRepository)
         ],
@@ -99,13 +32,122 @@ class HomePage extends StatelessWidget {
             geolocationRepository: geolocationRepository, // context.read<GeolocationRepository>(),
             bookingTaxiRepository: bookingTaxiRepository, // context.read<BookingTaxiRepository>()
           ),
-          child: HomeView(),
+          child: HomePage(),
         ),
-        // BlocProvider(
-        //   create: ,
-        //   child: HomeView(),
-        // ) 
-      )
+      ));
+  }
+  
+  static Page page() => const MaterialPage<void>(child: HomePage());
+
+  @override
+  Widget build(BuildContext context) {
+    GMapBloc gmapBloc = GMapBloc(
+      geolocationRepository: context.read<GeolocationRepository>(),
+      bookingTaxiBloc: context.read<BookingTaxiBloc>()
+    );
+
+    print('HOME ... GMAP Bloc STate ${gmapBloc.state.status}');
+    return 
+      BlocProvider.value(   
+        value: gmapBloc,     
+      // create: (context) => GMapBloc(
+      //   geolocationRepository: context.read<GeolocationRepository>(),
+      //   bookingTaxiBloc: context.read<BookingTaxiBloc>()
+      // ),
+      child: HomeView() // GMapPage()
+    );
+
+    // return Scaffold(
+    //   extendBodyBehindAppBar: true,
+      
+    //   appBar: AppBar(
+    //     automaticallyImplyLeading: true,
+    //     title: Row(
+    //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //       children: [
+    //         ConstrainedBox(
+    //           constraints: BoxConstraints.tightFor(width: 40, height: 40),
+    //           child: ElevatedButton(
+    //             onPressed: () {},
+    //             style: ElevatedButton.styleFrom(
+    //               padding: EdgeInsets.all(0.0),
+    //               primary: Colors.white,
+    //               shape: CircleBorder(),
+    //               elevation: 8.0
+    //             ),
+    //             child: IconButton(
+    //               icon: Icon(Icons.sort),
+    //               color: Color(0xFFC6902E),
+    //               onPressed: () {},
+    //             )
+    //           )
+    //         ),
+    //         Text(
+    //           AppLocalizations.of(context)!.home,
+    //           style: TextStyle(
+    //             color: Colors.black,
+    //             fontWeight: FontWeight.bold
+    //           ),
+    //         ),
+    //         ConstrainedBox(
+    //           constraints: BoxConstraints.tightFor(width: 40, height: 40),
+    //           child: ElevatedButton(
+    //             onPressed: () {},
+    //             style: ElevatedButton.styleFrom(
+    //               // padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+    //               padding: EdgeInsets.all(0.0),
+    //               primary: Colors.white,
+    //               shape: CircleBorder(),
+    //               elevation: 8.0
+    //             ),
+    //             child: IconButton(
+    //               icon: Icon(
+    //                 gmapBloc.state.status == GMapStatus.home ?  Icons.exit_to_app : Icons.cancel
+    //               ),
+    //               color: Color(0xFFC6902E),
+    //               onPressed: () {},
+    //             )
+    //           )
+    //         )
+    //       ],
+    //     ),
+    //     elevation: 0.0,
+    //     backgroundColor: Colors.transparent,
+    //   ),
+    //   body: BlocProvider.value(   
+    //     value: gmapBloc,     
+    //   // create: (context) => GMapBloc(
+    //   //   geolocationRepository: context.read<GeolocationRepository>(),
+    //   //   bookingTaxiBloc: context.read<BookingTaxiBloc>()
+    //   // ),
+    //   child: HomeView() // GMapPage()
+    // )
+    // );
+  }
+}
+
+class _LeftAppbarButton extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<GMapBloc, GMapState>(
+      builder: (context, state) {
+        return ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.all(0.0),
+            primary: Colors.white,
+            shape: CircleBorder(),
+            elevation: 8.0
+          ),
+          child: IconButton(
+            icon: Icon(Icons.sort),
+            color: Color(0xFFC6902E),
+            onPressed: () {},
+          )
+        );
+      }
     );
   }
+
 }
--- lib/pages/home/home_view.dart
+++ lib/pages/home/home_view.dart
@@ -1,25 +1,86 @@
 import 'package:andi_taxi/blocs/booking_taxi/booking_taxi_bloc.dart';
 import 'package:andi_taxi/blocs/gmap/gmap_bloc.dart';
-import 'package:andi_taxi/pages/gmap/cubit/gmap_cubit.dart';
+// import 'package:andi_taxi/pages/gmap/cubit/gmap_cubit.dart';
 import 'package:andi_taxi/pages/gmap/view/gmap_page.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:andi_taxi/repository/gmap/geolocation_repository.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // return GMapPage();
-    return BlocProvider(
-      create: (context) => GMapBloc(
-        geolocationRepository: context.read<GeolocationRepository>(),
-        bookingTaxiBloc: context.read<BookingTaxiBloc>()
+    return BlocBuilder<GMapBloc, GMapState>(
+      builder: (context, state) {
+        return 
+     Scaffold(
+      extendBodyBehindAppBar: true,
+      
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            ConstrainedBox(
+              constraints: BoxConstraints.tightFor(width: 40, height: 40),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(0.0),
+                  primary: Colors.white,
+                  shape: CircleBorder(),
+                  elevation: 8.0
+                ),
+                child: IconButton(
+                  icon: Icon(Icons.sort),
+                  color: Color(0xFFC6902E),
+                  onPressed: () {},
+                )
+              )
+            ),
+            Text(
+              AppLocalizations.of(context)!.home,
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            ConstrainedBox(
+              constraints: BoxConstraints.tightFor(width: 40, height: 40),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  // padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+                  padding: EdgeInsets.all(0.0),
+                  primary: Colors.white,
+                  shape: CircleBorder(),
+                  elevation: 8.0
+                ),
+                child: IconButton(
+                  icon: Icon(
+                    state.status == GMapStatus.home ?  Icons.exit_to_app : Icons.cancel
+                  ),
+                  color: Color(0xFFC6902E),
+                  onPressed: () {},
+                )
+              )
+            )
+          ],
+        ),
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
       ),
-      child: GMapPage()
+      body: GMapPage()
     );
-    // return BlocProvider(
-    //   create: (_) => GMapCubit(context.read<GeolocationRepository>()),
-    //   child: GMapPage() // Will change according to the State of Home,
+      }
+    );
+    // BlocProvider(
+    //   create: (context) => GMapBloc(
+    //     geolocationRepository: context.read<GeolocationRepository>(),
+    //     bookingTaxiBloc: context.read<BookingTaxiBloc>()
+    //   ),
+    //   child: GMapPage()
     // );
   }
 }
